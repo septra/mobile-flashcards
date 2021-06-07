97b2fae91b6e839f77a23f267798c613ca5a0e79
Sahil Singh <sahilsingh90@gmail.com>
2021-06-08 01:37:21 +0530

    Implement Deck component basic functionality

--- components/Deck.js
+++ components/Deck.js
@@ -1,12 +1,69 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 
 export default function Deck(props) {
   return (
-    <View>
-      <Text>
+    <View style={styles.container}>
+      <Text style={{alignSelf:'center'}}>
         {props.route.params.deckId}
       </Text>
+      <Button text="Start Quiz"></Button>
+      <Button text="Add Question"></Button>
     </View>
   )
 }
+
+function Button({ onPress, text }) {
+    return (
+        <TouchableOpacity
+            style={Platform.OS == 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn}
+            onPress={onPress}
+        >
+            <Text style={styles.submitBtnText}>{text}</Text>
+        </TouchableOpacity>
+
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      padding: 20,
+  },
+  row: {
+      flexDirection: 'row',
+      flex: 1,
+      alignItems: 'center'
+  },
+  iosSubmitBtn: {
+      // backgroundColor: purple,
+      padding: 10,
+      borderRadius: 7,
+      height: 45,
+      marginLeft: 40,
+      marginRight: 40,
+  },
+  androidSubmitBtn: {
+      // backgroundColor: purple,
+      padding: 10,
+      paddingLeft: 30,
+      paddingRight: 30,
+      height: 45,
+      borderRadius: 2,
+      alignSelf: 'flex-end',
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  submitBtnText: {
+      // color: white,
+      fontSize: 22,
+      textAlign: 'center'
+  },
+  center: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 30,
+      marginRight: 30
+  }
+})
