ff691556dc8959abaaa4043642ed12e3c29bb2af
Sahil Singh <sahilsingh90@gmail.com>
2021-06-08 01:27:55 +0530

    Add dummy data

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 
-const decks = [
-  {name: 'Deck 1', id: 'deck1'},
-  {name: 'Deck 2', id: 'deck2'},
-]
+const decks = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      }
+    ]
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+}
 
 export default function DeckList(props) {
   return (
