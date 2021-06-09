9c245148c0f1db2334c2975d1ebee78267ded4bd
Sahil Singh <sahilsingh90@gmail.com>
2021-06-10 04:55:41 +0530

    Flesh out NewQuestion component

--- components/NewQuestion.js
+++ components/NewQuestion.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Text, View, SafeAreaView, TextInput } from 'react-native'
+import { addCardToDeck } from '../api';
+import { Button } from './Deck'
+import {CommonActions} from '@react-navigation/native';
 
-export default function NewQuestion() {
+export default function NewQuestion(props) {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const handleSubmit = () => {
+    const card = {
+      question,
+      answer
+    }
+    const title = props.route.params.deckId
+    addCardToDeck(title, card)
+    console.log(card)
+    props.navigation.dispatch(CommonActions.goBack({
+      key: 'Deck'
+    }))
+  }
+
   return (
-    <View>
-      <Text>
-        New Question
-      </Text>
-    </View>
-  )
+    <SafeAreaView>
+      <TextInput
+        onChangeText={setQuestion}
+        value={question}
+        placeholder="Question"
+      />
+      <TextInput
+        onChangeText={setAnswer}
+        value={answer}
+        placeholder="Answer"
+      />
+      <Button text="Submit" onPress={() => handleSubmit()} />
+    </SafeAreaView>
+  );
 }
