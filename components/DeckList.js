8831ab164e9adbbe5ea2bac416c0a49e1cfb7ed9
Sahil Singh <sahilsingh90@gmail.com>
2021-06-10 03:39:57 +0530

    Load initial data from API call

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,32 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
-
-const decks = {
-  React: {
-    title: 'React',
-    questions: [
-      {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces'
-      },
-      {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event'
-      }
-    ]
-  },
-  JavaScript: {
-    title: 'JavaScript',
-    questions: [
-      {
-        question: 'What is a closure?',
-        answer: 'The combination of a function and the lexical environment within which that function was declared.'
-      }
-    ]
-  }
-}
+import { getDecks } from '../api'
 
 export default function DeckList(props) {
+  const [decks, setDecks] = useState({})
+
+  useEffect(() => {
+    getDecks()
+      .then((data) => {
+        setDecks(data)
+      })
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>DeckList</Text>
