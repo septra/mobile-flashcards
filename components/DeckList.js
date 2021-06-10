fe59bd70004e5ac901451ad5c55aa6c545f3bdf4
Sahil Singh <sahilsingh90@gmail.com>
2021-06-10 15:29:52 +0530

    Add styling to Deck component

--- components/DeckList.js
+++ components/DeckList.js
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import { getDecks } from '../api'
 import { receiveDecks } from '../actions'
+import { blue, brown, purple, red, yellow } from '../colors'
 
 export default function DeckList(props) {
   const decks = useSelector(state => {
@@ -19,7 +20,6 @@ export default function DeckList(props) {
 
   return (
     <View style={styles.container}>
-      <Text>DeckList</Text>
       {Object.keys(decks).map((deckName) => {
         const deck = decks[deckName]
         return (
@@ -41,8 +41,8 @@ function DeckListItem({ deck, linkTo }) {
       style={styles.item} 
       onPress={linkTo}
     >
-        <Text>{deck.title}</Text>
-        <Text>Total Questions: {deck.questions.length}</Text>
+        <Text style={styles.deckHeading}>{deck.title}</Text>
+        <Text style={styles.deckDetail}>Questions: {deck.questions.length}</Text>
     </TouchableOpacity>
   )
 }
@@ -53,11 +53,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: brown
   },
   item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    color: yellow,
+    borderRadius: 10,
     height: 100,
     width: '90%',
     marginVertical: 10,
-    backgroundColor: 'orange',
+    backgroundColor: purple,
+  },
+  deckHeading: {
+    color: yellow,
+    fontSize: 30
+  },
+  deckDetail: {
+    color: '#F0B7A4',
   }
 })
