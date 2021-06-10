165c2a22e0fb1c939e50248459bbcc55fc922207
Sahil Singh <sahilsingh90@gmail.com>
2021-06-11 00:07:22 +0530

    Seed decks if not already present in local storage

--- api.js
+++ api.js
@@ -39,29 +39,36 @@ export function addCardToDeck(title, card) {
     })
 }
 
-AsyncStorage.setItem('mobile-flashcards', 
-  JSON.stringify({
-    'React': {
-      title: 'React',
-      questions: [
-        {
-          question: 'What is React?',
-          answer: 'A library for managing user interfaces'
-        },
-        {
-          question: 'Where do you make Ajax requests in React?',
-          answer: 'The componentDidMount lifecycle event'
-        }
-      ]
-    },
-    'JavaScript': {
-      title: 'JavaScript',
-      questions: [
-        {
-          question: 'What is a closure?',
-          answer: 'The combination of a function and the lexical environment within which that function was declared.'
-        }
-      ]
+// Seed data if not already there.
+AsyncStorage.getItem(KEY)
+  .then(JSON.parse)
+  .then((data) => {
+    if (data === null) {
+      AsyncStorage.setItem(KEY, 
+        JSON.stringify({
+          'React': {
+            title: 'React',
+            questions: [
+              {
+                question: 'What is React?',
+                answer: 'A library for managing user interfaces'
+              },
+              {
+                question: 'Where do you make Ajax requests in React?',
+                answer: 'The componentDidMount lifecycle event'
+              }
+            ]
+          },
+          'JavaScript': {
+            title: 'JavaScript',
+            questions: [
+              {
+                question: 'What is a closure?',
+                answer: 'The combination of a function and the lexical environment within which that function was declared.'
+              }
+            ]
+          }
+        })
+      )
     }
   })
-)
