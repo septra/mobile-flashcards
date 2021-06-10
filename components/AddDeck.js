2d6449a806d99411018fbec70824b58e619a94d6
Sahil Singh <sahilsingh90@gmail.com>
2021-06-10 19:30:50 +0530

    Style AddDeck component

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import { Text, View, SafeAreaView, TextInput } from 'react-native'
 import { saveDeckTitle } from '../api';
-import { Button } from './Deck'
 import {CommonActions} from '@react-navigation/native';
 import { addDeck } from '../actions';
 import { useDispatch } from 'react-redux';
+import { styles, Button } from './Deck'
 
 export default function AddDeck(props) {
   const [title, setTitle] = useState("");
@@ -21,13 +21,14 @@ export default function AddDeck(props) {
   }
 
   return (
-    <SafeAreaView>
-      <TextInput
-        onChangeText={setTitle}
-        value={title}
-        placeholder="Enter New Deck Title"
-      />
-      <Button text="Submit" onPress={() => handleSubmit()} />
-    </SafeAreaView>
+      <View style={[styles.container, {justifyContent: 'center'}]}>
+        <TextInput
+          onChangeText={setTitle}
+          value={title}
+          placeholder="Enter New Deck Title"
+          keyboardType='default'
+        />
+        <Button style={{marginVertical: 50, width: '90%'}} text="Submit" onPress={() => handleSubmit()} />
+      </View>
   );
 }
